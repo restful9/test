81e2a80d2861be060cbf80939ad4923b6366266d
Alessandro Verardo <alessandroverardo@Alessandros-MBP.station>
2019-02-15 17:34:47 +0100

    lul

---
+++ resources/js/script.js
@@ -0,0 +1,35 @@
+function animate() {
+  var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+
+  function resetValues() {
+    anime.set(".spark", {
+      translateX: function() {
+        return anime.random(0, width - 48);
+      },
+      translateY: 0,
+      opacity: 1
+    });
+
+    anime({
+      targets: ".spark",
+      translateY: ["-10vh", "-80vh"],
+      easing: "linear",
+      opacity: 0,
+      delay: anime.stagger(anime.random(800, 2000)),
+      duration: 3000,
+      complete: resetValues
+    });
+  }
+
+  anime({
+    targets: [".flame-light", ".heading .is-small"],
+    opacity: 0.5,
+    easing: "easeInOutQuad",
+    duration: 3000,
+    loop: true,
+    direction: 'alternate'
+  });
+  resetValues();
+}
+
+document.addEventListener("DOMContentLoaded", animate);
